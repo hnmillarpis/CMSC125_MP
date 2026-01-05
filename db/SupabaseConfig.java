161c8598ba6b2674ca3d6a4f72605b723f470d9e
Dale Yuan D. De Guzman <dddeguzman3@up.edu.ph>
2026-01-06 06:56:43 +0800

    Added required db parts of the project

---
+++ db/SupabaseConfig.java
@@ -0,0 +1,9 @@
+package db;
+
+public class SupabaseConfig {
+
+    public static final String SUPABASE_URL =
+            "https://YOUR_PROJECT_ID.supabase.co";
+
+    public static final String SUPABASE_API_KEY =
+            "YOUR_PUBLIC_ANON_KEY";
